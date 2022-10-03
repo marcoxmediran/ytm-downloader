a133235a709bff3140f62431a523a381c2710f7e
marcoxmediran <marcoxmediran@gmail.com>
2022-10-03 20:09:14 +0800

    Program now runs continuously until EOFError

--- main.py
+++ main.py
@@ -22,40 +22,42 @@ opts = {
 }
 
 def main():
-    # get song link via command line argument
-    arguments = len(sys.argv)
-    if (arguments != 2):
-        print("Usage: python main.py LINK")
-        exit(1)
+    while True:
+        try:
+            # extract song ID from song link
+            LINK = input("LINK: ") 
+            ID_START = LINK.find('=') + 1
+            ID_END = ID_START + 11
+            ID = LINK[ID_START:ID_END]
 
-    # extract song ID from song link
-    LINK = sys.argv[1]
-    ID = LINK[LINK.find('=') + 1:LINK.find('&')]
+            # download song and save name
+            with YoutubeDL(opts) as ydl:
+                ydl.download([ID])
+                info = ydl.extract_info(ID, download=False)
+                NAME = ydl.prepare_filename(info)[:-4] + "mp3"
 
-    # download song and save name
-    with YoutubeDL(opts) as ydl:
-        ydl.download([ID])
-        info = ydl.extract_info(ID, download=False)
-        NAME = ydl.prepare_filename(info)[:-4] + "mp3"
+            # download album art
+            cover = get('https://i.ytimg.com/vi/{}/maxresdefault.jpg'.format(ID))
+            open("cover.jpg","wb").write(cover.content)
 
-    # download album art
-    cover = get('https://i.ytimg.com/vi/{}/maxresdefault.jpg'.format(ID))
-    open("cover.jpg","wb").write(cover.content)
+            # crop image
+            cover = Image.open(r"cover.jpg")
+            cover = cover.crop((280, 0, 1000, 720))
+            cover = cover.save("cover.jpg")
 
-    # crop image
-    cover = Image.open(r"cover.jpg")
-    cover = cover.crop((280, 0, 1000, 720))
-    cover = cover.save("cover.jpg")
+            # add album art
+            song = MP3(NAME, ID3 = ID3)
+            song.tags.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open('cover.jpg','rb').read()))
+            song.save()
 
-    # add album art
-    song = MP3(NAME, ID3 = ID3)
-    song.tags.add(APIC(mime='image/jpeg',type=3,desc=u'Cover',data=open('cover.jpg','rb').read()))
-    song.save()
-
-    # cleanup
-    remove('cover.jpg')
-
-    print("Downloaded {}".format(NAME))
+            # cleanup
+            remove('cover.jpg')
+            print("Downloaded {}".format(NAME))
+        
+        except EOFError:
+            print()
+            print("Program exit")
+            exit(0)
 
 if __name__ == "__main__":
     main()
